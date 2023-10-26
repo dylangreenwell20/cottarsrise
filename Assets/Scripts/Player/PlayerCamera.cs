fa50640e1aab8acb1dafc636113311b5850a41fd
dylangreenwell20 <dylan.greenwell@students.plymouth.ac.uk>
2023-10-26 01:50:35 +0100

    Version 0.1
    Created the base game. Developed a basic movement system (walk and jump) and a basic test environment (a plane with 2 cubes to jump on).

---
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public float sensX; //camera sensitivity variables
+    public float sensY;
+
+    public Transform orientation; //orientation variable
+
+    float xRotation; //rotation variables
+    float yRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; //lock mouse in middle of screen
+        Cursor.visible = false; //invisible cursor
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.fixedDeltaTime * sensX; //variables for storing mouse input (x and y directions)
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.fixedDeltaTime * sensY;
+
+        yRotation += mouseX; //applying mouse rotation
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); //locks camera from looking straight up or straight down
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0); //rotating the camera
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0); //rotating the orientation
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")] //new header for movement category
+    public float moveSpeed; //float for movement speed
+
+    public float groundDrag; //ground drag variable
+
+    public float jumpForce; //variable for force of jump
+    public float jumpCooldown; //variable for jump cooldown
+    public float airMultiplier; //variable for air multiplier
+    bool readyToJump; //boolean variable if the player is ready to jump or not (true = can jump, false = cannot jump)
+
+    [Header("Keybinds")] //new header for player keybinds
+    public KeyCode jumpKey = KeyCode.Space; //jump key set to space bar on keyboard
+
+    [Header("Ground Check")] //new header for checking if player is on the ground
+    public Transform groundCheck; //transform variable for ground check
+    public float groundDistance = 0f; //float variable for distance to ground
+    public LayerMask whatIsGround; //layermask variable for what is the ground
+    bool grounded; //boolean variable for grounded (true = on ground, false = off ground)
+
+    //public float playerHeight; //float variable for player height
+    //public LayerMask whatIsGround; //layermask variable for what is the ground                ###old ground check code - replaced due to issues with jumping
+    //bool grounded; //boolean variable for grounded (true = on ground, false = off ground)
+
+    public Transform orientation; //orientation variable
+    float horizontalInput; //horizontal movement input variable
+    float verticalInput; //vertical movement input variable
+    Vector3 moveDirection; //movement direction variable
+    Rigidbody rb; //rigidbody variable
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>(); //assigning rigid body
+        rb.freezeRotation = true; //freeze rigid body rotation
+        readyToJump = true;
+    }
+
+    private void Update()
+    {
+        grounded = Physics.CheckSphere(groundCheck.position, groundDistance, whatIsGround); //check if the player is on the ground
+        //grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround); //check is player is on ground            ###old ground check code - replaced due to isses with jumping
+        MyInput(); //run MyInput function
+        SpeedControl(); //run SpeedControl function
+
+        if (grounded) //if player is on the ground
+        {
+            rb.drag = groundDrag; //apply ground drag to movement
+        }
+        else //if player is not on the ground
+        {
+            rb.drag = 0; //apply no drag to movement
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer(); //run MovePlayer function
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal"); //get horizontal input
+        verticalInput = Input.GetAxisRaw("Vertical"); //get vertical input
+
+        if(Input.GetKeyDown(jumpKey) && readyToJump && grounded) //if player has pressed jump key and the boolean variables readyToJump and grounded are both true
+        { //IF I WANT USER TO BE ABLE TO HOLD SPACE AND KEEP JUMPING CONSTANTLY, CHANGE "Input.GeyKeyDown" TO "Input.GetKey"
+            readyToJump = false; //set readyToJump to false
+            Jump(); //run Jump function
+            Invoke(nameof(ResetJump), jumpCooldown); //invoke the ResetJump function with jumpCooldown
+        }
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput; //move in the way the player is facing
+
+        if (grounded) //if player is on the ground
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force); //add movement force for ground movement
+        }
+        
+        else if (!grounded) //if player is not on the ground
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force); //add movement force for non-grounded movement
+        }
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z); //variable for current player speed
+
+        if(flatVel.magnitude > moveSpeed) //if player speed is faster than max speed
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed; //calculate what the max velocity is
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z); //apply max velocity to current player speed
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z); //reset the y velocity so player will always jump the same height
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse); //apply jump force to player
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true; //set readyToJump to true
+    }
+
+}
