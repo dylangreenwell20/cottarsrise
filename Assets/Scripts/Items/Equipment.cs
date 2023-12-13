ce40a9c4f746597765d67f3d8d7728c2197cde5f
dylangreenwell20 <dylan.greenwell@students.plymouth.ac.uk>
2023-12-13 00:26:14 +0000

    Version 0.26
    I created a simple menu UI for the game - this allows the user to start on a main menu and view either class select, settings or exit the game. On class select, the player can choose a class and then start the game.
    
    At the moment, this is all UI - the next step is to create the code to choose what weapon the player will start with depending on what class they picked.

--- Assets/Scripts/Items/Equipment.cs
+++ Assets/Scripts/Items/Equipment.cs
@@ -11,6 +11,12 @@ public class Equipment : Item
     public int armourModifier; //value armour will be increased by
     public int damageModifier; //value damage will be increased by
 
+    public bool isWeapon; //if equipment is a weapon
+    public bool isOffhand; //if equipment is an offhand
+    public bool is2Handed; //if weapon is two handed so an offhand cannot be equipped at the same time
+
+    public WeaponType weaponType; //create new enum so weapons can have a specified type
+
     public override void Use()
     {
         base.Use(); //call the base class for the use function
@@ -20,3 +26,5 @@ public class Equipment : Item
 }
 
 public enum EquipSlot { Head, Chest, Legs, Boots, Weapon, Offhand} //new enum to let gear have a specific equip slot
+
+public enum WeaponType { Null, Melee, Range, Mage} //new enum to let weapons have a specific weapon type
---
+++ Assets/Scripts/UI/SelectedClass.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectedClass : MonoBehaviour
+{
+    public Button warriorButton; //reference to warrior button
+    public Button archerButton; //reference to archer button
+    public Button mageButton; //reference to mage button
+    public Button playButton; //reference to play button
+
+    public Color selectedColour; //reference to colour for selected button
+    public Color defaultColour; //reference to colour for non-selected button
+
+    public GameObject mainMenu; //reference to main menu
+    public GameObject classMenu; //reference to class menu
+
+    public bool warriorSelected; //if warrior has been selected or not
+    public bool archerSelected; //if archer has been selected or not
+    public bool mageSelected; //if mage has been selected or not
+
+    public bool classSelected; //if a class has been selected or not
+
+    private void Awake()
+    {
+        classSelected = false; //no class selected by default
+    }
+
+    private void Update()
+    {
+        if (classSelected) //if a class has been selected
+        {
+            playButton.gameObject.SetActive(true); //show play button
+        }
+        else //else if no class has been selected
+        {
+            playButton.gameObject.SetActive(false); //hide play button
+        }
+    }
+
+    public void SelectWarrior() //select warrior and deselect other classes
+    {
+        //set warrior as selected and deselect other classes
+        warriorSelected = true;
+        archerSelected = false;
+        mageSelected = false;
+
+        ColourButtons(); //update colour of button outlines
+
+        classSelected = true; //a class has been selected
+    }
+
+    public void SelectArcher() //select archer and deselect other classes
+    {
+        //set archer as selected and deselect other classes
+        warriorSelected = false;
+        archerSelected = true;
+        mageSelected = false;
+
+        ColourButtons(); //update colour of button outlines
+
+        classSelected = true; //a class has been selected
+    }
+
+    public void SelectMage() //select mage and deselect other classes
+    {
+        //set mage as selected and deselect other classes
+        warriorSelected = false;
+        archerSelected = false;
+        mageSelected = true;
+
+        ColourButtons(); //update colour of button outlines
+
+        classSelected = true; //a class has been selected
+    }
+
+    public void BackToMenu()
+    {
+        classMenu.SetActive(false); //hide class menu
+        mainMenu.SetActive(true); //show main menu
+
+        //deselect all classes
+        warriorSelected = false;
+        archerSelected = false;
+        mageSelected = false;
+
+        classSelected = false; //no class is selected
+
+        ColourButtons(); //colour buttons back to default colour
+    }
+
+    public void ColourButtons()
+    {
+        if (warriorSelected) //if warrior selected
+        {
+            warriorButton.GetComponent<Outline>().effectColor = selectedColour; //set outline of button to a specific colour
+        }
+        else //else if warrior not selected
+        {
+            warriorButton.GetComponent<Outline>().effectColor = defaultColour; //set outline of button to a specific colour
+        }
+        
+        if (archerSelected) //if archer selected
+        {
+            archerButton.GetComponent<Outline>().effectColor = selectedColour; //set outline of button to a specific colour
+        }
+        else //else if archer not selected
+        {
+            archerButton.GetComponent<Outline>().effectColor = defaultColour; //set outline of button to a specific colour
+        }
+
+        if (mageSelected) //if mage selected
+        {
+            mageButton.GetComponent<Outline>().effectColor = selectedColour; //set outline of button to a specific colour
+        }
+        else //else if mage not selected
+        {
+            mageButton.GetComponent<Outline>().effectColor = defaultColour; //set outline of button to a specific colour
+        }
+    }
+}
