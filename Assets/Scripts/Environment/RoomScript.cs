eb625904afc2d896e7d069f4102449c0302341ed
dylangreenwell20 <dylan.greenwell@students.plymouth.ac.uk>
2024-04-04 11:56:40 +0100

    Version 0.37
    Created a new room template which I will use to create a variety of dungeon rooms.
    
    I also deleted an old asset pack I had installed which I do not need any more.

--- Assets/Scripts/Environment/RoomScript.cs
+++ Assets/Scripts/Environment/RoomScript.cs
@@ -7,11 +7,13 @@ public class RoomScript : MonoBehaviour
 {
     public GameObject[] walls; //wall prefabs - 0 = north, 1 = south, 2 = east, 3 = west
     public GameObject[] doors; //door prefabs - same indexing as above
+    public GameObject[] door_walls; //door wall prefabs - same indexing as above
 
     public void UpdateRoom(bool[] status)
     {
         for(int i = 0; i < status.Length; i++) //for length of status array
         {
+            door_walls[i].SetActive(status[i]); //set door wall status to value of status[i]
             doors[i].SetActive(status[i]); //set door status to value of status[i]
             walls[i].SetActive(!status[i]); //set wall status to inverse value of status[i]
         }
