b84fd4bd67f2b55d91d0a7563ee6494e9b98156c
dylangreenwell20 <dylan.greenwell@students.plymouth.ac.uk>
2024-04-04 10:10:49 +0100

    Version 0.36
    FIXED THE BUG!!!!! Was due to something in the CheckNeighbour offset - I changed this and also the size of the dungeon from 31x31 to 15x15 as I implemented a system to change direction of generation if the same direction is picked twice (for example, if 2 rooms are generated in the north direction and it tries to generate a third, it will delete the north neighbour from the list and pick either east or west).
    
    I plan on creating bigger prefab rooms, but will keep them all the same size for simplicity with the offset.

--- Assets/Scripts/Environment/DungeonGenerator.cs
+++ Assets/Scripts/Environment/DungeonGenerator.cs
@@ -22,16 +22,12 @@ public class DungeonGenerator : MonoBehaviour
 
     public int genOrder = 0; //for testing
 
-    //increments as it goes back? fix this
-    //check to see if the room has already been visited - if it has, do not increment the room counter
+    private bool directionCheck; //loop to check if room is generating to a previously-made room
 
-    //store previous room direction from current room (e.g: if room goes from east to west, store that previous room is located to the east)
-    //if next direction picked is the same as previous room location (e.g: direction picked is east and previous room is located to the east)
-    //then proceed with room door generation, but do not increment the rooms created variable)
-    //do not forget to store the previous room location again!!!
+    public int previousGenDirection; //previous direction a room was generated in (0 = north, 1 = east, 2 = south, 3 = west)
 
-    //so it can generate the 10th room, but just generates the door and not the room
-    //and because random trolls me, i might have to store the previous direction and if it matches multiple times, change the direction so it isnt a long line
+    public bool sameDirectionTwice; //if 2 rooms were generated in the same direction - used because unity.random is (for some reason) not random all the time
+    //and using this bool i can prevent every other dungeon from being a straight line north (yes this happens too much it should be impossible)
 
     private void Start()
     {
@@ -72,15 +68,17 @@ public class DungeonGenerator : MonoBehaviour
             }
         }
 
-        int currentCell = startPos; //define start position
+        previousGenDirection = 5; // set to 5 as this is not a valid position so it will not effect previous direction checks later on
 
-        //Vector2 currentCellPos = startPosition;
+        int currentCell = startPos; //define start position
 
         Stack<int> path = new Stack<int>(); //create new stack
 
         int roomsNumber = 0; //number of rooms generated so far
 
-        while(roomsNumber < 10) //while less than 10 rooms have been generated
+        sameDirectionTwice = false; //set to false for same direction checks
+
+        while (roomsNumber < 10) //while less than 10 rooms have been generated
         {
             board[currentCell].visited = true; //current cell has been visited
 
@@ -102,57 +100,189 @@ public class DungeonGenerator : MonoBehaviour
             {
                 path.Push(currentCell); //push currentCell to stack
 
-                int newCell = neighbours[Random.Range(0, neighbours.Count)]; //pick random neighbour from neighbours list
+                directionCheck = false; //set to false for room direction check loop
 
-                if(newCell > currentCell) //if new cell is going south or east
+                while (!directionCheck) //while previous direction has not been checked yet (room cannot generate to the direction it came from)
                 {
-                    if((newCell - 1) == currentCell) //check if room is east
+                    int randomIndex = Random.Range(0, neighbours.Count); //pick a random index from the list of neighbours
+
+                    int newCell = neighbours[randomIndex]; //pick random neighbour from neighbours list
+
+                    for(int i = 0; i < neighbours.Count; i++)
                     {
-                        board[currentCell].status[2] = true; //set east door of current cell to true
-                        currentCell = newCell; //update current cell to new cell
-                        board[currentCell].status[3] = true; //set west door of current cell to true
-
-                        Debug.Log("room made east - " + roomsNumber);
-
-                        roomsNumber++; //a room was created
-                    }
-                    else //else if the room is going south
-                    {
-                        board[currentCell].status[1] = true; //set south door of current cell to true
-                        currentCell = newCell; //update current cell to new cell
-                        board[currentCell].status[0] = true; //set north door  of current cell to true
-
-                        Debug.Log("room made south - " + roomsNumber);
-
-                        roomsNumber++; //a room was created
+                        Debug.Log(neighbours[i]);
                     }
 
-                    
-                }
-                else //the new cell is going north or west
-                {
-                    if ((newCell + 1) == currentCell) //check if room is west
+                    Debug.Log("count: " + neighbours.Count);
+                    Debug.Log("index: " + randomIndex);
+                    Debug.Log("cell: " + newCell);
+
+                    if (board[newCell].visited) //if cell already visited
                     {
-                        board[currentCell].status[3] = true; //set west door of current cell to true
-                        currentCell = newCell; //update current cell to new cell
-                        board[currentCell].status[2] = true; //set east door of current cell to true
-
-                        Debug.Log("room made west - " + roomsNumber);
-
-                        roomsNumber++; //a room was created
+                        Debug.Log("visited already"); //debug and make another neighbour be picked
+                        neighbours.RemoveAt(randomIndex); //remove neighbour from the list
                     }
-                    else //else if the room is going south
+                    else //else if neighbour hasnt been visited
                     {
-                        board[currentCell].status[0] = true; //set north door of current cell to true
-                        currentCell = newCell; //update current cell to new cell
-                        board[currentCell].status[1] = true; //set south door of current cell to true
+                        if (newCell > currentCell) //if new cell is going south or east
+                        {
+                            if ((newCell - 1) == currentCell) //check if room is east
+                            {
+                                Debug.Log("pre: " + previousGenDirection + ", " + sameDirectionTwice);
 
-                        Debug.Log("room made north - " + roomsNumber);
+                                if (previousGenDirection == 1 && sameDirectionTwice == true) //if rooms have been created in the same direction twice before and is going in the same direction
+                                {
+                                    Debug.Log("3rd time going in the same direction - change the path");
+                                    neighbours.RemoveAt(randomIndex); //remove that neighbour from the list
 
-                        roomsNumber++; //a room was created
+                                    sameDirectionTwice = false; //set back to false
+                                }
+                                else
+                                {
+                                    board[currentCell].status[2] = true; //set east door of current cell to true
+                                    currentCell = newCell; //update current cell to new cell
+                                    board[currentCell].status[3] = true; //set west door of current cell to true
+
+                                    Debug.Log("room made east - " + roomsNumber);
+
+                                    if(previousGenDirection == 1) //if previous room was created to the east
+                                    {
+                                        sameDirectionTwice = true; //two rooms have been generated in the same direction
+                                        Debug.Log("east twice!");
+                                    }
+                                    else
+                                    {
+                                        sameDirectionTwice = false;
+                                    }
+
+                                    previousGenDirection = 1; //future rooms can tell the last direction was east
+
+                                    //Debug.Log("current: " + previousGenDirection);
+
+                                    roomsNumber++; //a room was created
+
+                                    directionCheck = true; //end direction check loop
+                                }
+                            }
+                            else //else if the room is going south
+                            {
+                                Debug.Log("pre: " + previousGenDirection + ", " + sameDirectionTwice);
+
+                                if (previousGenDirection == 2 && sameDirectionTwice == true) //if previous direction was south and two have been made in that direction
+                                {
+                                    Debug.Log("3rd time going in the same direction - change the path");
+                                    neighbours.RemoveAt(randomIndex); //remove that neighbour from the list
+
+                                    sameDirectionTwice = false; //set back to false
+                                }
+                                else
+                                {
+                                    board[currentCell].status[1] = true; //set south door of current cell to true
+                                    currentCell = newCell; //update current cell to new cell
+                                    board[currentCell].status[0] = true; //set north door  of current cell to true
+
+                                    Debug.Log("room made south - " + roomsNumber);
+
+                                    if(previousGenDirection == 2) //if previous room was created to the south
+                                    {
+                                        sameDirectionTwice = true; //two rooms have been generated in the same direction
+                                        Debug.Log("south twice!");
+                                    }
+                                    else
+                                    {
+                                        sameDirectionTwice = false;
+                                    }
+
+                                    previousGenDirection = 2; //future rooms can tell the last direction was south
+
+                                    //Debug.Log("current: " + previousGenDirection);
+
+                                    roomsNumber++; //a room was created
+
+                                    directionCheck = true; //end direction check loop
+                                }
+                            }
+                        }
+                        else //the new cell is going north or west
+                        {
+                            if ((newCell + 1) == currentCell) //check if room is west
+                            {
+                                Debug.Log("pre: " + previousGenDirection + ", " + sameDirectionTwice);
+
+                                if (previousGenDirection == 3 && sameDirectionTwice == true)
+                                {
+                                    Debug.Log("3rd time going in the same direction - change the path");
+                                    neighbours.RemoveAt(randomIndex); //remove that neighbour from the list
+
+                                    sameDirectionTwice = false; //set back to false
+                                }
+                                else
+                                {
+                                    board[currentCell].status[3] = true; //set west door of current cell to true
+                                    currentCell = newCell; //update current cell to new cell
+                                    board[currentCell].status[2] = true; //set east door of current cell to true
+
+                                    Debug.Log("room made west - " + roomsNumber);
+
+                                    if(previousGenDirection == 3)
+                                    {
+                                        sameDirectionTwice = true;
+                                        Debug.Log("west twice!");
+                                    }
+                                    else
+                                    {
+                                        sameDirectionTwice = false;
+                                    }
+
+                                    previousGenDirection = 3; //future rooms can tell the last direction was west
+
+                                    //Debug.Log("current: " + previousGenDirection);
+
+                                    roomsNumber++; //a room was created
+
+                                    directionCheck = true; //end direction check loop
+                                }
+                            }
+                            else //else if the room is going north
+                            {
+                                Debug.Log("pre: " + previousGenDirection + ", " + sameDirectionTwice);
+
+                                if (previousGenDirection == 0 && sameDirectionTwice == true)
+                                {
+                                    Debug.Log("3rd time going in the same direction - change the path");
+                                    neighbours.RemoveAt(randomIndex); //remove that neighbour from the list
+
+                                    sameDirectionTwice = false; //set back to false
+                                }
+                                else
+                                {
+                                    board[currentCell].status[0] = true; //set north door of current cell to true
+                                    currentCell = newCell; //update current cell to new cell
+                                    board[currentCell].status[1] = true; //set south door of current cell to true
+
+                                    Debug.Log("room made north - " + roomsNumber);
+
+                                    if(previousGenDirection == 0)
+                                    {
+                                        sameDirectionTwice = true;
+                                        Debug.Log("north twice!");
+                                    }
+                                    else
+                                    {
+                                        sameDirectionTwice = false;
+                                    }
+
+                                    previousGenDirection = 0; //future rooms can tell the last direction was north
+
+                                    //Debug.Log("current: " + previousGenDirection);
+
+                                    roomsNumber++; //a room was created
+
+                                    directionCheck = true; //end direction check loop
+                                }
+                            }
+                        }
                     }
-
-                    //roomsNumber++; //a room was created
                 }
             }
         }
@@ -164,24 +294,24 @@ public class DungeonGenerator : MonoBehaviour
     {
         List<int> neighbours = new List<int>(); //create neighbours list
 
-        if(cell - size.x >= 0 && !board[Mathf.FloorToInt(cell - size.x)].visited) //check if there is a neighbour cell to the north which isnt visited
+        if(cell - 15 >= 0 && !board[Mathf.FloorToInt(cell - 15)].visited) //check if there is a neighbour cell to the north which isnt visited
         {
-            neighbours.Add(Mathf.FloorToInt(cell - size.x)); //add neighbour to list
+            neighbours.Add(Mathf.FloorToInt(cell - 15)); //add neighbour to list
         }
 
-        if (cell + size.x < board.Count && !board[Mathf.FloorToInt(cell + size.x)].visited) //check if there is a neighbour cell to the south which isnt visited
-        {
-            neighbours.Add(Mathf.FloorToInt(cell + size.x)); //add neighbour to list
-        }
-
-        if((cell + 1) % size.x != 0 && !board[Mathf.FloorToInt(cell + size.x)].visited) //check if there is a neighbour cell to the east which isnt visited
+        if((cell + 1) % 15 != 0 && !board[Mathf.FloorToInt(cell + 1)].visited) //check if there is a neighbour cell to the east which isnt visited
         {
             neighbours.Add(Mathf.FloorToInt(cell + 1)); //add neighbour to list
         }
 
-        if (cell % size.x != 0 && !board[Mathf.FloorToInt(cell + size.x)].visited) //check if there is a neighbour cell to the west which isnt visited
+        if (cell + 15 < board.Count && !board[Mathf.FloorToInt(cell + 15)].visited) //check if there is a neighbour cell to the south which isnt visited
         {
-            neighbours.Add(Mathf.FloorToInt(cell - 1)); //add neighbour to list
+            neighbours.Add(Mathf.FloorToInt(cell + 15)); //add neighbour to list
+        }
+
+        if ((cell - 1) % 15 != 0 && !board[Mathf.FloorToInt(cell - 1)].visited) //check if there is a neighbour cell to the west which isnt visited
+        {
+            neighbours.Add(Mathf.FloorToInt(cell + 1)); //add neighbour to list
         }
 
         return neighbours;
